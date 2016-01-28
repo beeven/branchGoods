9573c0fa9288fe025e2e0c3cc5fef57c15492952
snikelin <snikelin@163.com>
2016-01-28 17:30:20 +0900

    edit nextpage

--- app/views/main.js
+++ app/views/main.js
@@ -2,7 +2,7 @@
 
 var angular = require("angular");
 
-module.exports = angular.module('branchApp.goods',[require('angular-ui-bootstrap'),require('angular-busy').name,require("./../widget/pagination").name]);
+module.exports = angular.module('branchApp.goods',[require('angular-ui-bootstrap'),require('angular-busy/dist/angular-busy.min').name,require("./../widget/pagination").name]);
 
 require('./search');
 require('./goodList');
